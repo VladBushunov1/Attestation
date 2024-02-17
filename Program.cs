9dc7f2fb8617d975015faf8a921f8824b780418b
VladBushunov1 <vladbushunovWork@gmail.com>
2024-02-17 13:52:26 +0300

    Создали цикл, который занесёт элементы из первого массива во второй

--- Program.cs
+++ Program.cs
@@ -1,5 +1,13 @@
 ﻿string [] my_str = ["3 nums", "1_n", "t","a lot of symbols", "123", "1234",":-)"];
 string [] new_str = new string [my_str.Length];
 int j = 0;
-
+for (int i = 0; i < my_str.Length; i++)
+{
+    if (my_str[i].Length <= 3)
+    {
+    new_str[j] = my_str[i];
+    Console.Write($"{new_str[j]} ");
+    j++;
+    }
+}
 
